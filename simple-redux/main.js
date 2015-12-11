6eb8bc2ddb9002ce6b47826f3e329c20eef64a14
cryptolemming <aliayoub22@gmail.com>
2015-12-11 16:19:41 -0500

    complete uniloc example and start redux store representation

--- simple-redux/main.js
+++ simple-redux/main.js
@@ -11,3 +11,16 @@ function onHashChange() {
 
 // handle the browser nagivation events
 window.addEventListener('haschange', onHashChange, false);
+
+// example named routes definitions with uniloc
+var Routes = uniloc({
+	// route name: route parameters denotd by :
+	root: 'GET /',
+	documentList: 'GET /:userID/:documentID',
+	documentEdit: 'GET /:userID/documentID/edit',
+})
+
+// use ROUTES.lookup(url) returns object where name: 'route name', options: {route parameters}
+// use ROUTES.generate('route name', {route parameters}) to generate a URL
+
+// now we have a location for the user, we put that in a Redux store
