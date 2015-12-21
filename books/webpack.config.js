2e5ca3ea2ceb8ad7f03b8d395461f7ad38acad0c
cryptolemming <aliayoub22@gmail.com>
2015-12-21 15:54:04 -0500

    ignore npm-debug

---
+++ books/webpack.config.js
@@ -0,0 +1,15 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+	entry: [
+		'./index.js'
+	],
+
+	output: {
+		path: __dirname,
+		filename: 'bundle.js'
+	},
+
+	
+};
