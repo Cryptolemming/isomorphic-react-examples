999feef64df02ef5481f85320f4005930c3bffce
cryptolemming <aliayoub22@gmail.com>
2015-12-21 14:08:44 -0500

    created books folder to practice, and initial Books component

---
+++ books/index.js
@@ -0,0 +1,33 @@
+var Books = React.createClass({
+	propTypes: {
+		books: React.PropTypes.array.isRequired,
+	},
+
+	render: function() {
+		var bookElements = this.props.books
+			.map(function(book) {
+				React.createElement('li', {},
+					React.createElement('h2', {}, book.title),
+					React.createElement('p', {}, book.author)
+				)
+			});
+
+		return (
+			React.createElement('div', {}, 
+				React.createElement('ul', {}, bookElements)
+			)
+		);
+	},	
+});
+
+var data = [
+	{title: 'Life of Pi', author: 'Yann Martel'},
+	{title: 'Ender"s Game', author: 'Orson Scott Card'}
+]
+
+ReactDOM.render(
+	React.createElement(Books, {
+		books: data
+	}),
+	document.getElementById('index')
+);
