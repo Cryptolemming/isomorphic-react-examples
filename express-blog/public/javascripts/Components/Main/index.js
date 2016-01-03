5461dd4ed9ca086469a4b62fafc3791d1a43699d
cryptolemming <aliayoub22@gmail.com>
2016-01-03 13:49:47 -0500

    created Main component shell for nav and layout

---
+++ express-blog/public/javascripts/Components/Main/index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var Main = React.createClass({
+
+	render: function() {
+		return (
+
+		)
+	}
+});
+
+ReactDOM.render(
+	React.createElement(Main),
+	document.getElementById('main')
+);
