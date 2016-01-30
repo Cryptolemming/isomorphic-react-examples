bbbcef9444145b2f4ac44772f0ae60920d9320f5
cryptolemming <aliayoub22@gmail.com>
2016-01-30 18:51:31 -0500

    added new admin routing for Projects and Music as well as the respective admin views

--- express-blog/routes/admin.js
+++ express-blog/routes/admin.js
@@ -53,32 +53,6 @@ router
 		});
 	})	
 
-	/**
-	.post('/dashboard', requireLogin, function(req, res) {
-		// Depending on which form was filled, create new entry and redirect
-		if (req.body.postTitle) {
-			
-		} else if (req.body.songTitle) {
-			new Song({title: req.body.songTitle, arist: req.body.artist, link: req.body.link})
-			.save(function(err, song) {
-				res.redirect('/music');
-			});
-		} else if (req.body.projectName) {
-			new Project({
-				name: req.body.projectName,
-				picture: req.body.picture,
-				languages: req.body.languages,
-				summary: req.body.summary
-			})
-			.save(function(err, project) {
-				res.redirect('/projects');
-			});
-		} else {
-			res.redirect('/');
-		}
-	})
-**/
-
 	// Dashboard
 	.get('/dashboard', requireLogin, function(req, res) {
 		res.render('admin/dashboard');
@@ -114,7 +88,7 @@ router
 			if (err)
 				console.log(err);
 			else if (!post)
-				console.log('not found get');
+				console.log('not found');
 			else
 				res.render('admin/posts/post', {title: post.title, body: post.body, title_slug: post.title_slug});
 		});
@@ -126,13 +100,12 @@ router
 			if (err)
 				console.log(err);
 			else if (!post)
-				console.log('not found put');
+				console.log('not found');
 			else 
 				post.title = req.body.title;
 				post.body = req.body.body;
 				console.log(post);
 				post.save(function(err) {
-					console.log(post);
 					if(err)
 						console.log(err);
 					else
@@ -146,7 +119,7 @@ router
 		var query = {'title_slug': req.params.title_slug};
 		Post.findOne(query, function(err, post) {
 			if (!post) {
-				console.log('not found delete');
+				console.log('not found');
 			}
 			else {
 			 	post.remove(function(err) {
@@ -163,10 +136,171 @@ router
 	
 
 	// Dashboard Projects
-	
+			// view index
+	.get('/dashboard/projects', function(req, res) {
+		var projectData = Project.find().sort({_id: -1});
+		projectData.find(function(err, projects) {
+			res.render('admin/projects/index', 
+				{projects: projects}
+			);
+		});
+	})
+
+		// new project
+	.get('/dashboard/projects/new', function(req, res) {
+		res.render('admin/projects/new');
+	})
+
+	.post('/dashboard/projects/new', function(req, res) {
+		new Project(
+			{
+				name: req.body.name,
+				picture: req.body.picture,
+				languages: req.body.languages,
+				summary: req.body.summary
+			})
+			.save(function(err, project) {
+				if(err) {
+					console.log(err);
+				};
+				res.redirect('/admin/dashboard/projects');
+			});
+	})
+
+		// edit project
+	.get('/dashboard/projects/:name_slug', function(req, res) {
+		var query = {'name_slug': req.params.name_slug};
+		Project.findOne(query, function(err, project) {
+			if (err)
+				console.log(err);
+			else if (!project)
+				console.log('not found');
+			else
+				res.render('admin/projects/project', 
+					{
+						name: req.body.name,
+						picture: req.body.picture,
+						languages: req.body.languages,
+						summary: req.body.summary
+					});
+		});
+	})
+
+	.put('/dashboard/projects/:name_slug', function(req, res) {
+		var query = {'name_slug': req.params.name_slug};
+		Project.findOne(query, function(err, project) {
+			if (err)
+				console.log(err);
+			else if (!project)
+				console.log('not found');
+			else 
+				project.name = req.body.name;
+				project.picture = req.body.picture;
+				project.languages = req.body.languages;
+				project.summary = req.body.summary;
+				project.save(function(err) {
+					if(err)
+						console.log(err);
+					else
+						res.redirect('/admin/dashboard/projects/' + project.name_slug);
+				});
+		});
+	})
+
+		// delete project
+	.delete('/dashboard/projects/:name_slug', function(req, res) {
+		var query = {'name_slug': req.params.name_slug};
+		Project.findOne(query, function(err, project) {
+			if (!project) {
+				console.log('not found');
+			}
+			else {
+			 	project.remove(function(err) {
+					if(err) {
+						console.log(err);
+					}
+					else {
+						res.redirect('/admin/dashboard/projects');
+					}
+				});
+			}
+		});
+	})
 
 	// Dashboard Music
-	
+		// view index
+	.get('/dashboard/music', function(req, res) {
+		var songData = Song.find().sort({_id: -1});
+		songData.find(function(err, songs) {
+			res.render('admin/music/index', 
+				{songs: songs}
+			);
+		});
+	})
+
+	// new song
+	.get('/dashboard/music/new', function(req, res) {
+		res.render('admin/music/new');
+	})
+
+	.post('/dashboard/music/new', function(req, res) {
+		new Song({title: req.body.title, link: req.body.link})
+			.save(function(err, song) {
+				res.redirect('/admin/dashboard/music');
+			});
+	})
+
+	// edit song
+	.get('/dashboard/music/:title_slug', function(req, res) {
+		var query = {'title_slug': req.params.title_slug};
+		Song.findOne(query, function(err, song) {
+			if (err)
+				console.log(err);
+			else if (!song)
+				console.log('not found');
+			else
+				res.render('admin/music/song', {title: song.title, link: song.link, title_slug: song.title_slug});
+		});
+	})
+
+	.put('/dashboard/music/:title_slug', function(req, res) {
+		var query = {'title_slug': req.params.title_slug};
+		Song.findOne(query, function(err, song) {
+			if (err)
+				console.log(err);
+			else if (!post)
+				console.log('not found');
+			else 
+				song.title = req.body.title;
+				song.link = req.body.link;
+				song.save(function(err) {
+					if(err)
+						console.log(err);
+					else
+						res.redirect('/admin/dashboard/music/' + song.title_slug);
+				});
+		});
+	})
+
+	// delete song
+	.delete('/dashboard/music/:title_slug', function(req, res) {
+		var query = {'title_slug': req.params.title_slug};
+		Song.findOne(query, function(err, song) {
+			if (!song) {
+				console.log('not found');
+			}
+			else {
+			 	song.remove(function(err) {
+					if(err) {
+						console.log(err);
+					}
+					else {
+						res.redirect('/admin/dashboard/music');
+					}
+				});
+			}
+		});
+	})
 
 	// Logout
 	.get('/logout', function(req, res) {
